c921d57879ffb179120b03f1ce991b57c52934f7
jerzybalcer <jurekbalcer@gmail.com>
2025-03-14 11:23:32 +0100

    change prefix of env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 declare interface Env {
   readonly NODE_ENV: string;
-  readonly NG_SPOTIFY_CLIENT_ID: string;
-  readonly NG_SPOTIFY_REDIRECT_URI: string;
+  readonly NG_APP_SPOTIFY_CLIENT_ID: string;
+  readonly NG_APP_SPOTIFY_REDIRECT_URI: string;
 }
 
 declare interface ImportMeta {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    SpotifyClientId: import.meta.env.NG_SPOTIFY_CLIENT_ID,
-    SpotifyRedirectUri: import.meta.env.NG_SPOTIFY_REDIRECT_URI,
+    SpotifyClientId: import.meta.env.NG_APP_SPOTIFY_CLIENT_ID,
+    SpotifyRedirectUri: import.meta.env.NG_APP_SPOTIFY_REDIRECT_URI,
 };
 
 console.log(environment);
