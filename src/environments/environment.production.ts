45e07c0963b9a579bba96e5962291b159f9da107
jerzybalcer <jurekbalcer@gmail.com>
2025-03-14 11:09:58 +0100

    use default env as prod

--- src/environments/environment.production.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: true,
-    SpotifyClientId: import.meta.env.NG_SPOTIFY_CLIENT_ID,
-    SpotifyRedirectUri: import.meta.env.NG_SPOTIFY_REDIRECT_URI,
-};
-
-console.log(environment);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    SpotifyClientId: '',
-    SpotifyRedirectUri: ''
+    production: true,
+    SpotifyClientId: import.meta.env.NG_SPOTIFY_CLIENT_ID,
+    SpotifyRedirectUri: import.meta.env.NG_SPOTIFY_REDIRECT_URI,
 };
+
+console.log(environment);
